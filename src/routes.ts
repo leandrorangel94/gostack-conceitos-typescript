b469b8dfab74075f1180df3ee7801f6d031bf75e
leandrorangel94 <rangel94@outlook.com>
2020-09-02 20:51:50 -0300

    tipando objeto e vetores

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,11 @@
 import { Request, Response} from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: 'teste@gmail.com', password: 'teste',
+    techs: ['Node.js', {title:'Javascript', experience: 100}]
+  });
+  
   return response.json({message: 'Hello World'});
 }
